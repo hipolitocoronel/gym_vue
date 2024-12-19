e6a9f8320072347d8d6524869bcc4db440289f7d
Juan3234ux2 <juancoronelfacu@gmail.com>
2024-12-18 21:31:12 -0300

    solucion error

--- src/views/pages/auth/Login.vue
+++ src/views/pages/auth/Login.vue
@@ -1,163 +1,73 @@
 <script setup>
-import FloatingConfigurator from '@/components/FloatingConfigurator.vue';
-import router from '@/router';
-import pb from '@/service/pocketbase.js';
-import { useIndexStore } from '@/storage/index.js';
-import { Form } from '@primevue/forms';
-import { zodResolver } from '@primevue/forms/resolvers/zod';
-import { ref } from 'vue';
-import { z } from 'zod';
+import FloatingConfigurator from "@/components/FloatingConfigurator.vue";
+import router from "@/router";
+import pb from "@/service/pocketbase.js";
+import { useIndexStore } from "@/storage/index.js";
+import { Form } from "@primevue/forms";
+import { zodResolver } from "@primevue/forms/resolvers/zod";
+import { ref } from "vue";
+import { z } from "zod";
 
 const store = useIndexStore();
 
 const checked = ref(false);
 
 const initialValues = ref({
-    email: '',
-    password: ''
+  email: "",
+  password: "",
 });
 
 const resolver = zodResolver(
-    z.object({
-        email: z.string().email({ message: 'Correo electrónico inválido' }),
-        password: z.string().min(3, { message: 'Mínmo 3 caracteres.' })
-    })
+  z.object({
+    email: z.string().email({ message: "Correo electrónico inválido" }),
+    password: z.string().min(3, { message: "Mínmo 3 caracteres." }),
+  })
 );
 
 const onFormSubmit = async (e) => {
-    if (e.valid) {
-        try {
-            const authData = await pb
-                .collection('users')
-                .authWithPassword(e.values.email, e.values.password);
+  if (e.valid) {
+    try {
+      const authData = await pb
+        .collection("users")
+        .authWithPassword(e.values.email, e.values.password);
 
-            // guardando informacion de usuario
-            store.setUserLogged(authData.record);
+      // guardando informacion de usuario
+      store.setUserLogged(authData.record);
 
-            // redireccion
-            router.push({ name: 'dashboard' });
-        } catch (error) {
-            console.error(error);
-            alert(error);
-        }
+      // redireccion
+      router.push({ name: "dashboard" });
+    } catch (error) {
+      console.error(error);
+      alert(error);
     }
+  }
 };
 </script>
 
 <template>
-    <FloatingConfigurator />
-    <div
-        class="bg-surface-50 dark:bg-surface-950 flex items-center justify-center min-h-screen min-w-[100vw] overflow-hidden"
-    >
-        <div class="flex flex-col items-center justify-center">
-            <div
-                style="
-                    border-radius: 56px;
-                    padding: 0.3rem;
-                    background: linear-gradient(
-                        180deg,
-                        var(--primary-color) 10%,
-                        rgba(33, 150, 243, 0) 30%
-                    );
-                "
-            >
-                <div
-                    class="w-full px-8 py-20 bg-surface-0 dark:bg-surface-900 sm:px-20"
-                    style="border-radius: 53px"
-                >
-                    <div class="mb-8 text-center">
-                        <div class="mb-4 text-3xl font-medium text-surface-900 dark:text-surface-0">
-                            Bienvenido de nuevo!
-                        </div>
-                        <span class="font-medium text-muted-color">Ingrese para continuar</span>
-                    </div>
-
-                    <Button class="w-full mb-6" severity="contrast" variant="outlined">
-                        <img
-                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png"
-                            alt=""
-                            width="24px"
-                        />
-                        <span class="ml-1"> Continuar con Google </span>
-                    </Button>
-
-                    <Form v-slot="$form" :initialValues :resolver @submit="onFormSubmit">
-                        <label
-                            for="email"
-                            class="block mb-2 text-lg font-medium text-surface-900 dark:text-surface-0"
-                        >
-                            Correo
-                        </label>
-                        <div class="mb-8">
-                            <InputText
-                                name="email"
-                                id="email"
-                                type="text"
-                                placeholder="Correo electrónico"
-                                class="w-full md:w-[30rem] mb-2"
-                                v-model="email"
-                            />
-
-                            <Message
-                                v-if="$form.email?.invalid"
-                                severity="error"
-                                size="small"
-                                variant="simple"
-                            >
-                                {{ $form.email.error.message }}
-                            </Message>
-                        </div>
-
-                        <label
-                            for="password"
-                            class="block mb-2 text-lg font-medium text-surface-900 dark:text-surface-0"
-                        >
-                            Contraseña
-                        </label>
-
-                        <div class="mb-4">
-                            <Password
-                                name="password"
-                                id="password"
-                                v-model="password"
-                                placeholder="Contraseña"
-                                :toggleMask="true"
-                                class="mb-2"
-                                fluid
-                                :feedback="false"
-                            >
-                            </Password>
-
-                            <Message
-                                v-if="$form.password?.invalid"
-                                severity="error"
-                                size="small"
-                                variant="simple"
-                            >
-                                {{ $form.password.error.message }}
-                            </Message>
-                        </div>
-
-                        <div class="flex items-center justify-between gap-8 mt-2 mb-8">
-                            <div class="flex items-center">
-                                <Checkbox
-                                    v-model="checked"
-                                    id="rememberme1"
-                                    binary
-                                    class="mr-2"
-                                ></Checkbox>
-                                <label for="rememberme1">Recordar</label>
-                            </div>
-                            <span
-                                class="ml-2 font-medium text-right no-underline cursor-pointer text-primary"
-                                >Olvidaste tu contraseña?</span
-                            >
-                        </div>
-                        <Button class="w-full mt-2 font-bold" type="submit">
-                            <b class="text-lg tracking-wide">Iniciar sesión</b>
-                        </Button>
-                    </Form>
-                </div>
+  <FloatingConfigurator />
+  <div
+    class="bg-surface-50 dark:bg-surface-950 flex items-center justify-center min-h-screen min-w-[100vw] overflow-hidden"
+  >
+    <div class="flex flex-col items-center justify-center">
+      <div
+        style="
+          border-radius: 56px;
+          padding: 0.3rem;
+          background: linear-gradient(
+            180deg,
+            var(--primary-color) 10%,
+            rgba(33, 150, 243, 0) 30%
+          );
+        "
+      >
+        <div
+          class="w-full px-8 py-20 bg-surface-0 dark:bg-surface-900 sm:px-20"
+          style="border-radius: 53px"
+        >
+          <div class="mb-8 text-center">
+            <div class="mb-4 text-3xl font-medium text-surface-900 dark:text-surface-0">
+              Bienvenido de nuevo!
             </div>
             <span class="font-medium text-muted-color">Ingrese para continuar</span>
           </div>
