c7494bea43010df720f3a9ab661053e7ab617f07
Hipolito Coronel <hipolitocoronel1522@gmail.com>
2024-12-19 15:26:05 -0300

    func async de get users con paginacion

--- src/views/pages/Usuarios.vue
+++ src/views/pages/Usuarios.vue
@@ -1,13 +1,39 @@
 <script setup>
 import pb from '@/service/pocketbase';
+import { useToast } from 'primevue/usetoast';
 import { onMounted, ref } from 'vue';
+const toast = useToast();
 
 const users = ref([]);
+const first = ref(0); // corresponde al current page
+const loading = ref(false);
+const totalRecords = ref(0);
+const rowsPerPage = ref(10);
 
-onMounted(async () => {
-    const result = await pb.collection('users').getList(1, 10);
-    users.value = result.items;
-});
+onMounted(() => getUsuarios({ first: first.value, rows: rowsPerPage.value }));
+
+const getUsuarios = async (event) => {
+    try {
+        // Parámetros de la consulta
+        first.value = event.first;
+        rowsPerPage.value = event.rows;
+
+        loading.value = true;
+        const result = await pb.collection('users').getList(first.value + 1, rowsPerPage.value);
+        totalRecords.value = result.totalItems;
+        users.value = result.items;
+    } catch (error) {
+        console.log(error);
+        toast.add({
+            severity: 'error',
+            summary: 'Operación fallida',
+            detail: 'No se pudo obtener los usuarios',
+            life: 3000
+        });
+    } finally {
+        loading.value = false;
+    }
+};
 </script>
 
 <template>
@@ -27,7 +53,19 @@ onMounted(async () => {
             </Button>
         </div>
 
-        <DataTable :value="users">
+        <DataTable
+            :value="users"
+            paginator
+            :rows="rowsPerPage"
+            :lazy="true"
+            :totalRecords="totalRecords"
+            :first="first"
+            :loading="loading"
+            @page="getUsuarios"
+            paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+            :rowsPerPageOptions="[10, 30, 50]"
+            currentPageReportTemplate="Mostrando {last} de {totalRecords} cuentas"
+        >
             <Column field="name" header="Nombre"></Column>
             <Column field="email" header="Correo"></Column>
             <Column field="phone" header="Teléfono"></Column>
