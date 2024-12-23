ed0eda77a382bf33e9d360692723fd6c7cc0a9a7
Hipolito Coronel <hipolitocoronel1522@gmail.com>
2024-12-23 15:41:13 -0300

    detalle

--- src/views/pages/auth/ForgotPassword.vue
+++ src/views/pages/auth/ForgotPassword.vue
@@ -5,17 +5,25 @@ import pb from '@/service/pocketbase';
 import { Form } from '@primevue/forms';
 import { zodResolver } from '@primevue/forms/resolvers/zod';
 import { useToast } from 'primevue/usetoast';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
+import { useRoute } from 'vue-router';
 import { z } from 'zod';
 const { isDarkTheme } = useLayout();
 
+const route = useRoute();
 const toast = useToast();
 const loading = ref(false);
 const initialValues = ref({ email: '', password: '', passwordConfirm: '' });
+const step = computed(() => (route.query?.token?.length > 0 ? 2 : 1));
 
 const resolver = zodResolver(
     z.object({
-        email: z.string().email({ message: 'Correo electrónico inválido' })
+        email: z.string().email({ message: 'Correo electrónico inválido' }),
+        password: z
+            .string()
+            .min(3, { message: 'Mínimo 3 caracteres.' })
+            .max(20, { message: 'No debe exceder 20 caracteres.' }),
+        passwordConfirm: z.string().min(3, { message: 'Mínimo 3 caracteres.' })
     })
 );
 
@@ -23,7 +31,15 @@ const onFormSubmit = async (e) => {
     if (e.valid) {
         try {
             loading.value = true;
-            await pb.collection('users').requestPasswordReset(e.values.email);
+            step.value == 1
+                ? await pb.collection('users').requestPasswordReset(e.values.email)
+                : await pb
+                      .collection('users')
+                      .confirmPasswordReset(
+                          route.query.token,
+                          e.values.password,
+                          e.values.passwordConfirm
+                      );
 
             // redireccion
         } catch (error) {
@@ -75,15 +91,19 @@ const onFormSubmit = async (e) => {
 
                         <div class="mt-6 mb-8">
                             <h1 class="mb-2 text-2xl font-bold text-center">
-                                ¿Olvidaste tu contraseña?
+                                {{
+                                    step == 1
+                                        ? '¿Olvidaste tu contraseña?'
+                                        : 'Restablecer contraseña'
+                                }}
                             </h1>
-                            <p class="text-center text-muted-color">
+                            <p class="text-center text-muted-color" v-if="step == 1">
                                 No te preocupes, te enviaremos instrucciones de reinicio.
                             </p>
                         </div>
 
                         <Form v-slot="$form" :initialValues :resolver @submit="onFormSubmit">
-                            <div class="flex flex-col gap-2 mb-3" v-auto-animate>
+                            <div class="flex flex-col gap-2 mb-3" v-auto-animate v-if="step == 1">
                                 <label for="email">Correo electrónico</label>
                                 <InputText
                                     name="email"
@@ -104,11 +124,66 @@ const onFormSubmit = async (e) => {
                                 </Message>
                             </div>
 
+                            <section v-else>
+                                <div class="flex flex-col gap-2 mb-5" v-auto-animate>
+                                    <label for="password">
+                                        Nueva contraseña
+                                        <span class="text-red-400">*</span>
+                                    </label>
+                                    <Password
+                                        name="password"
+                                        id="password"
+                                        v-model="password"
+                                        placeholder="* * * * * * * * *"
+                                        :toggleMask="true"
+                                        class="w-full md:w-[30rem] mb-2"
+                                        fluid
+                                        :feedback="false"
+                                    >
+                                    </Password>
+
+                                    <Message
+                                        v-if="$form.password?.invalid"
+                                        severity="error"
+                                        size="small"
+                                        variant="simple"
+                                    >
+                                        {{ $form.password.error.message }}
+                                    </Message>
+                                </div>
+
+                                <div class="flex flex-col gap-1 mb-5" v-auto-animate>
+                                    <label for="passwordConfirm">
+                                        Confirmar contraseña
+                                        <span class="text-red-400">*</span>
+                                    </label>
+                                    <Password
+                                        name="passwordConfirm"
+                                        id="passwordConfirm"
+                                        v-model="passwordConfirm"
+                                        placeholder="* * * * * * * * *"
+                                        :toggleMask="true"
+                                        class="w-full md:w-[30rem] mb-2"
+                                        fluid
+                                        :feedback="false"
+                                    />
+
+                                    <Message
+                                        v-if="$form.passwordConfirm?.invalid"
+                                        severity="error"
+                                        size="small"
+                                        variant="simple"
+                                    >
+                                        {{ $form.passwordConfirm.error.message }}
+                                    </Message>
+                                </div>
+                            </section>
+
                             <Button
                                 class="w-full mt-2 font-bold"
                                 type="submit"
                                 :loading="loading"
-                                label="Restablecer contraseña"
+                                :label="(step == 1 ? 'Restablecer' : 'Guardar') + ' contraseña'"
                             />
                         </Form>
 
@@ -119,6 +194,7 @@ const onFormSubmit = async (e) => {
                             severity="secondary"
                             as="router-link"
                             to="/auth/login"
+                            v-if="step == 1"
                         >
                             <i class="pi pi-arrow-left"></i>
                             Volver al inicio
