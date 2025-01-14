e6171e80f027b80863d87f095e76260de2e77553
Hipolito Coronel <hipolitocoronel1522@gmail.com>
2025-01-14 15:19:19 -0300

    cambio slogan

--- src/components/landing/HeroWidget.vue
+++ src/components/landing/HeroWidget.vue
@@ -1,14 +1,14 @@
 <template>
     <div id="hero" class="flex flex-col px-4 overflow-hidden bg-black lg:px-20">
         <div></div>
-        <div class="max-w-3xl mx-auto mt-24 text-center md:mt-36">
+        <div class="max-w-[57rem] mx-auto mt-24 text-center md:mt-36">
             <h1
                 class="text-[42px] sm:text-6xl md:text-7xl lg:text-8xl lg:min-h-52 font-extrabold leading-tight bg-gradient-to-r from-white to-gray-700 bg-clip-text text-transparent animate-blurred-fade-in animate-delay-300"
             >
-                Simplifica Tu Negocio Fitness
+                Impulsa tu gimnasio al siguiente nivel
             </h1>
             <p
-                class="max-w-2xl mx-auto mt-6 text-sm font-normal leading-normal text-gray-200 md:text-xl md:mt-10 animate-blurred-fade-in animate-delay-400"
+                class="max-w-3xl mx-auto mt-6 text-sm font-normal leading-normal text-gray-200 md:text-xl md:mt-10 animate-blurred-fade-in animate-delay-400"
             >
                 Optimiza tus operaciones, impulsa el crecimiento y mejora la experiencia de tus
                 clientes con nuestro sistema integral de gestión para gimnasios.
